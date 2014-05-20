020949a58b62f934660fecc07086a27343e08803
Michael Roup <mike@Viss-iMac.local>
2014-05-20 13:15:24 -0400

    generate csv with the tif files only. some iffy parallel support.

--- utils/build_csv.pl
+++ utils/build_csv.pl
@@ -4,7 +4,7 @@ use strict;
 use File::Find;
 
 my $iterations = 100;
-my $output_suffix = "_Reg2010.jpg";
+my $output_suffix = "_Reg2010.tif";
 our @landmarks;
 our %data;
 
@@ -12,17 +12,16 @@ find(\&wanted, '.');
 sub wanted {
   if (/[.]ldm$/) {
     push @landmarks, $File::Find::name
-  } elsif (/\d+-(.+)-(\d{4})[.]jpg$/) {
+  } elsif (/\d+-(.+)-(\d{4})[.]tif$/i) {
     $data{$1}->{$2} = $File::Find::name
   }
 }
 
 open(my $csv, ">", "registration_batch.csv");
-
 for my $ldm (@landmarks) {
   $ldm =~ /\d+-(.+)-(\d{4})/;
   my ($page, $year) = ($1, $2);
   my $output_image = $data{$page}->{$year};
-  $output_image =~ s/[.]jpg$/$output_suffix/;
+  $output_image =~ s/[.]tif$/$output_suffix/i;
   print $csv "$ldm, $data{$page}->{2010}, $data{$page}->{$year}, $output_image, $iterations\n";
 }
