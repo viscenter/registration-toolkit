a2445b90d679c7dc99d87349ace301a077042a07
Nikki Bentley <snbe225@g.uky.edu>
2014-07-29 11:12:52 -0400

    add comments and clean up code

--- utils/auto_align_tiles.pl
+++ utils/auto_align_tiles.pl
@@ -1,45 +1,62 @@
 #!/usr/bin/perl
+
+# Usage: auto_align_tiles    CSV file
+# This program automatically generates a single landmark file for 
+#an image based on 5 tiles of the image.
+
 use warnings;
 use strict;
 use String::Util 'trim';
 use File::Spec;
 
+#defining to clear the whitespace from the left side of a string
 sub ltrim { my $s = shift; $s =~ s/^\s+//;       return $s };
 
 my $floatname;
 my $fixedyear;
 
-#image wd hd image wd hd
+# CSV format:image1 width height image2 width height
+#The width and height numbers are the differences between the 
+#original image and each tile
 
 while (<>){
+
   chomp;
   my @commands = split ", ";
 
-  my $wholefixedimage = $commands[0];
-  $wholefixedimage = ltrim($wholefixedimage);
-  my($wholefixed, $ext)= split(/\./, $wholefixedimage);
-  ($fixedyear, my $fixedname) = split(/\//, $wholefixed);
+  if($commands[5]){
+    #assigning and splitting arguments, if you want to switch 
+    #which image is moving/fixed, swap the index numbers 
 
-  my $fixed_wd = $commands[1];
-  my $fixed_hd = $commands[2];
+    my $wholefixedimage = $commands[0];
+    $wholefixedimage = ltrim($wholefixedimage);
+    my($wholefixed, $ext)= split(/\./, $wholefixedimage);
+    ($fixedyear, my $fixedname) = split(/\//, $wholefixed);
 
-  my $wholefloatimage = $commands[3];
-  $wholefloatimage = ltrim($wholefloatimage);
+    my $fixed_wd = $commands[1];
+    my $fixed_hd = $commands[2];
 
-  my ($wholefloat, $ext2) = split(/\./, $wholefloatimage);
-  (my $floatyear, $floatname) = split(/\//, $wholefloat);
+    my $wholefloatimage = $commands[3];
+    $wholefloatimage = ltrim($wholefloatimage);
+    my ($wholefloat, $ext2) = split(/\./, $wholefloatimage);
+    (my $floatyear, $floatname) = split(/\//, $wholefloat);
 
-  my $float_wd = $commands[4];
-  my $float_hd = $commands[5];
+    my $float_wd = $commands[4];
+    my $float_hd = $commands[5];
 
-  chdir("./$floatyear");
-  mkdir("./landmarks") unless (-d "./landmarks");
-  chdir("./landmarks");
+    #change directories to the floating image year, and creating a landmarks folder
+    chdir("./$floatyear");
+    mkdir("./landmarks") unless (-d "./landmarks");
+    chdir("./landmarks");
 
-  open (my $tilelandmarks, ">", "tilelandmarks.ldm") or die "coudldn't open tilelandmarks.ldm";
-  my $scale = 700;
-
-  for my $tile(1..5){
+    #open file to make landmarks to
+    open (my $tilelandmarks, ">", "tilelandmarks.ldm") or die "coudldn't open tilelandmarks.ldm";
+  
+    #scale downsizes the image
+    my $scale = 700;
+    
+    #get absolute file path for tiles
+    for my $tile(1..5){
     chdir('..');
     chdir('..');
     my $floattemppath = "./$wholefloat/$floatname" . "_tile$tile" . ".jpg";
@@ -47,44 +64,61 @@ while (<>){
 
     my $floatimage = File::Spec->rel2abs($floattemppath);
     my $fixedimage = File::Spec->rel2abs($fixedtemppath);
-;
+    
+    #create key file
     my $fixedkey = $fixedimage;
     $fixedkey =~ s/[.]jpg$/.key/i;
     my $floatkey = $floatimage;
     $floatkey =~  s/[.]jpg$/.key/i;
 
+    #make sure both images exist
     die "$fixedimage doesn't exist\n" if not defined -e $fixedimage;
     die "$floatimage doesn't exist\n" if not defined -e $floatimage;
-
-   chdir("./$floatyear/landmarks");
-
+    
+    chdir("./$floatyear/landmarks");
+    
+    #AUTOPANO and ALIGNMENT
     print "Starting Keys\n";
     `generatekeys $fixedimage $fixedkey $scale`;
     print "Finished 1 Key, 1 more to go\n";
     `generatekeys $floatimage $floatkey $scale`;
     print "Finished 2 keys, starting autopano\n";
-    `autopano --maxmatches '3'  --integer-coordinates alignment.pto $fixedkey $floatkey`;
+    `autopano --maxmatches '7'  --integer-coordinates alignment.pto $fixedkey $floatkey`; 
     print "finished autopano\n";
 
     open(my $alignment, "<", "alignment.pto") or die "couldn't open alignment.pto";
 
+
+    #write results of autopano/alignment to file for each tile
     for(<$alignment>){
       print $tilelandmarks "$1 $2 $3 $4 tile$tile\n" if  /x(\d+) y(\d+) X(\d+) Y(\d+)/;
     }
   }
-  close($tilelandmarks);
-  open($tilelandmarks, "<", "tilelandmarks.ldm") or die "couldn't open tilelandmarks.ldm";
-  my $ldmfilename = "$floatname" . "_Reg$fixedyear" . ".ldm";
-  open(my $landmarks, ">", "$ldmfilename") or die "couldn't open new landmarks file";
-  my ($t1 ,$t2, $t3, $t4, $t5) = (0,0,0,0,0);
-  while(<$tilelandmarks>){
+    close($tilelandmarks);
+
+    #write landmarks from individual tile document into one document, fixing them for the original image
+    
+    #reopen the tile landmark file
+    open($tilelandmarks, "<", "tilelandmarks.ldm") or die "couldn't open tilelandmarks.ldm";
+    #create final file name
+    my $ldmfilename = "$floatname" . "_Reg$fixedyear" . ".ldm";
+    #open final landmark file
+    open(my $landmarks, ">", "$ldmfilename") or die "couldn't open new landmarks file";
+    #set all number of landmarks to 0
+    #(keeps count for a balanced number of landmarks per tile)
+    my ($t1 ,$t2, $t3, $t4, $t5) = (0,0,0,0,0);
+
+    while(<$tilelandmarks>){
       my @coords = split " ";
+      #adjust each point to fit the original image, instead of the tile
+      #tile 1-upper left corner-accurate, no changes to the coordinates
       if ($coords[4] =~ /tile1/){
         if ($t1 <= 1){
           print $landmarks "$coords[0] $coords[1] $coords[2] $coords[3]\n";
           $t1 += 1;
         }
       }
+      #tile 2-upper right corner- add width difference to x values
       elsif($coords[4] =~ /tile2/){
         if ($t2 <= 1){
           my $x = $coords[0];
@@ -95,6 +129,8 @@ while (<>){
           $t2+=1;
         }
       }
+      #tile 3-lower right corner-add width difference to x values and 
+      #     height difference to y values
       elsif($coords[4] =~ /tile3/){
         if($t3 <= 1){
           my $x = $coords[0]; 
@@ -109,6 +145,7 @@ while (<>){
           $t3+=1;
         }
       }
+      #tile 4-lower left corner-add height difference to y values
       elsif($coords[4] =~ /tile4/){
         if($t4 <= 1){
           my $y = $coords[1]; 
@@ -119,6 +156,7 @@ while (<>){
           $t4+=1;
         }  
       }
+      #tile 5-center tile-add half of width to x and half of height to y values
       elsif($coords[4] =~ /tile5/){
         if($t5 <= 1){
           my $x = $coords[0];   
@@ -133,9 +171,14 @@ while (<>){
           $t5+=1;
         }  
       }
+      #else-error-do nothing
       else{}
     }
-  unlink($tilelandmarks);
-  chdir('..');
-  chdir('..');
+    #Close tile landmarks and change back to starting directory
+    unlink($tilelandmarks);
+    chdir('..');
+    chdir('..');
+  }
+
+  else{}
 }
