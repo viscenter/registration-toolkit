5e1b6a5ac7b44dae65e09c322a1cafab296e4be8
Stephen Parsons <srpa226@uky.edu>
2019-05-01 14:54:44 -0400

    Don't freak out if we can't find VC

--- cmake/FindDependencies.cmake
+++ cmake/FindDependencies.cmake
@@ -38,7 +38,7 @@ find_package(TIFF REQUIRED)
 # Optional #
 ############
 
-find_package(VC 2.13)
+find_package(VC 2.13 CONFIG)
 
 # If this option is set, then use all optional dependencies
 option(PROJ_USE_ALL "Use all optional third-party libs" off)
