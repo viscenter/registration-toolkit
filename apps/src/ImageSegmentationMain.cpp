d27956f0388e7841607b1331fc8602294ee44fef
Sammy Stampley <sammy@Viss-iMac-3.local>
2019-11-26 10:30:55 -0500

    Clang-Formatted all files

--- apps/src/ImageSegmentationMain.cpp
+++ apps/src/ImageSegmentationMain.cpp
@@ -2,29 +2,28 @@
 
 using namespace cv;
 
+int main(int argc, char* argv[])
+{
 
-int main(int argc, char* argv[]) {
+    // TESTING PURPOSE
+    cv::Mat input =
+        cv::imread(samples::findFile("DesigniTest2.png"), cv::IMREAD_COLOR);
 
-
-    //TESTING PURPOSE
-    cv::Mat input = cv::imread(samples::findFile("simpleShapes.jpg") , cv::IMREAD_COLOR);
-
-    //Checks for valid input
-    if( input.empty() )
-    {
-        std::cout <<  "Could not open or find the image" << std::endl ;
+    // Checks for valid input
+    if (input.empty()) {
+        std::cout << "Could not open or find the image" << std::endl;
         return EXIT_FAILURE;
     }
-    //TESTING PURPOSE
+    // TESTING PURPOSE
 
     DisegniSegmenter a;
     a.setInputImage(input);
     auto fragmentedImage = a.compute();
 
-    //TESTING PURPOSE
+    // TESTING PURPOSE
     cv::namedWindow("Fragmented Image", 0);
     cv::imshow("Fragmented Image", fragmentedImage[0]);
     cv::waitKey(0);
     return EXIT_SUCCESS;
-    //TESTING PURPOSE
+    // TESTING PURPOSE
 }
--- rtlib/include/rt/DisegniSegmenter.hpp
+++ rtlib/include/rt/DisegniSegmenter.hpp
@@ -1,37 +1,39 @@
 #pragma once
 
-//C++ Standard Libraries
-#include <vector>
-#include <utility>
+// C++ Standard Libraries
 #include <string>
+#include <utility>
+#include <vector>
 
-//External Libraries
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/core/utility.hpp>
-#include <opencv2/opencv.hpp>
+// External Libraries
 #include <iostream>
-#include <stdio.h>
 #include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/utility.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
-//Image Blur Library
-#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+// Image Blur Library
 #include <opencv2/highgui/highgui_c.h>
-
+#include <opencv2/imgproc/imgproc.hpp>
 
 /*
- * This algorithm takes an input Mat image and inserts it into a private class member.
- * This private class member is ran through a watershed algorithm where it's differing color
- * intensities are used to segment the image. After being ran through the watershed algorithm,
- * the image is segmented into different pixel subregions that can be manipulated and displayed
- * through various methods. Next the watersheded image is ran through a split and merge function.
- * The split and merge function took advantage of the differing pixel subregions to insert them into
- * each of their own Mat image. Finally the split Mat images are inserted into a vector and returned
- * as a vector. This vector being the output of the algorithm.
+ * This algorithm takes an input Mat image and inserts it into a private class
+ * member. This private class member is ran through a watershed algorithm where
+ * it's differing color intensities are used to segment the image. After being
+ * ran through the watershed algorithm, the image is segmented into different
+ * pixel subregions that can be manipulated and displayed through various
+ * methods. Next the watersheded image is ran through a split and merge
+ * function. The split and merge function took advantage of the differing pixel
+ * subregions to insert them into each of their own Mat image. Finally the split
+ * Mat images are inserted into a vector and returned as a vector. This vector
+ * being the output of the algorithm.
  */
-class DisegniSegmenter {
+class DisegniSegmenter
+{
 
 public:
     void setInputImage(cv::Mat i);
@@ -44,8 +46,3 @@ private:
     cv::Mat watershed_image_();
     std::vector<cv::Mat> split_merge_(cv::Mat watershededImage);
 };
-
-
-
-
-
