2649792d3556a55b92d1f8d65c09d9a57fddb29e
Anthony Tamasi <anthonytamasi@Viss-iMac-2.local>
2019-12-12 11:29:00 -0500

    Added documentation for registration package

--- rtlib/include/rt/RegPkg.hpp
+++ rtlib/include/rt/RegPkg.hpp
@@ -8,29 +8,47 @@
 #include <string>
 
 namespace rt {
+/**
+ * @class RegPkg
+ * @brief Interface for an organization structure for image
+ * registration projects
+ * @ingroup regpkg
+ */
 class RegPkg
 {
 
-    public:
-        RegPkg(); //Default constructor
+public:
+    /** Constructor */
+    RegPkg();
+    /** Constructor overload */
+    RegPkg(std::string root_dir);  // Constructor with setting root directory,
+                                   // linking to a regpkg
 
-        RegPkg(std::string root_dir); //Constructor with setting root directory, linking to a regpkg
+    /** @brief Return list of data inputs */
+    auto getInputs() const {};  // Return list of inputs. What should the type
+                                // be?? General data class????
 
-        auto getInputs() const {}; //Return list of inputs. What should the type be?? General data class????
+    /** @brief Return a specific data input */
+    auto getInput(std::string input_dir)
+        const {};  // Get one of the input data objects from the existing list
 
-        auto getInput(std::string input_dir) const {}; //Get one of the input data objects from the existing list
+    /** @brief Add a data input */
+    auto addInput(std::string input_dir){};
 
-        auto addInput(std::string input_dir) {};
+    /** @brief Remove a data input from the list */
+    auto removeInput(std::string input_dir){};
 
-        auto removeInput(std::string input_dir) {};
+    /** @brief Load in a registration package from a file directory */
+    auto loadRegPkg(std::string root_dir){};
 
-        auto loadRegPkg(std::string root_dir) {};
+private:
+    /**  Root directory of the registration package*/
+    std::string rootPath_;
 
+    /** List of all input data objects this registration package contains */
+    auto inputDataList_;
 
-    private:
-        std::string rootPath_; //root directory of this regpkg
-        auto inputDataList_; //some list of all the input data objects. What are those object types?
-
-        void save() {};
+    /** Save this registration package */
+    void save(){};
 };
 }
